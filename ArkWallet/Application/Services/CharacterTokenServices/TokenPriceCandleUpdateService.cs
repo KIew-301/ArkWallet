522a25db64dfc0ed225cbfaa24722319bf542477
Klew <nikita.losev200440@gmail.com>
2026-06-25 19:44:08 +1000

    feat(TokenPriceCandleUpdateService): updated

--- ArkWallet/Application/Services/CharacterTokenServices/TokenPriceCandleUpdateService.cs
+++ ArkWallet/Application/Services/CharacterTokenServices/TokenPriceCandleUpdateService.cs
@@ -1,22 +1,40 @@
 ﻿using ArkWallet.Domain.Entities;
+using ArkWallet.Domain.Exceptions;
 using ArkWallet.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
-namespace ArkWallet.Application.Services.CharacterTokenServices
+namespace ArkWallet.Application.Services.CharacterTokenServices;
+
+internal class TokenPriceCandleUpdateService(ArkWalletDbContext dbContext, TimeProvider timeProvider, ILogger<TokenPriceCandleUpdateService> logger)
 {
-    internal class TokenPriceCandleUpdateService(ArkWalletDbContext dbContext)
+    public async Task<TokenPriceCandleUpdateResult> UpdateTokenPriceCandleAsync(string symbol, decimal newPrice)
     {
-        public async Task UpdateTokenPriceCandleAsync(string symbol, decimal newPrice)
+        const int SavingTimeFrameInMinute = 1;
+
+        try
         {
+            var token = await dbContext.CharacterTokens.FirstOrDefaultAsync(c => c.Symbol == symbol);
+            if (token == null)
+                return TokenPriceCandleUpdateResult.Fail("Токен не найден");
+
             var lastCandle = await dbContext.PriceCandles
                 .Where(c => c.CharacterTokenId == symbol)
                 .OrderByDescending(c => c.Timestamp)
                 .FirstOrDefaultAsync();
 
+            var dateTimeNow = timeProvider.GetUtcNow().UtcDateTime;
+
             if (lastCandle == null)
             {
-                lastCandle = PriceCandle.CreateNew(symbol, newPrice, DateTime.UtcNow);
-                await dbContext.PriceCandles.AddAsync(lastCandle);
+                var newCandle = PriceCandle.CreateNew(symbol, newPrice, dateTimeNow);
+                await dbContext.PriceCandles.AddAsync(newCandle);
+            }
+            else if (lastCandle.Timestamp.AddMinutes(SavingTimeFrameInMinute) <= dateTimeNow)
+            {
+                var newCandle = PriceCandle.CreateNew(symbol, lastCandle.ClosePrice, dateTimeNow);
+                newCandle.Update(newPrice);
+                await dbContext.PriceCandles.AddAsync(newCandle);
             }
             else
             {
@@ -25,6 +43,31 @@ namespace ArkWallet.Application.Services.CharacterTokenServices
             }
 
             await dbContext.SaveChangesAsync();
+            return TokenPriceCandleUpdateResult.Ok();
+        }
+        catch (DomainException ex)
+        {
+            return TokenPriceCandleUpdateResult.Fail($"Ошибка бизнес-логики: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, $"Ошибка сохранения баланса в истории");
+            var innerMessage = ex.InnerException?.Message ?? ex.Message;
+            return TokenPriceCandleUpdateResult.Fail($"Внутренняя ошибка сервера: {innerMessage}");
         }
     }
 }
+
+internal record TokenPriceCandleUpdateResult(
+    bool IsSuccess, string message)
+{
+    public static TokenPriceCandleUpdateResult Ok()
+    {
+        return new(true, "Данные цене сохранены в историю");
+    }
+
+    public static TokenPriceCandleUpdateResult Fail(string message)
+    {
+        return new(false, message);
+    }
+};
