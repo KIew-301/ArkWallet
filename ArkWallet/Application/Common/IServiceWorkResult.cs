2c4c3db47ab878ea94784018e6097341fd95afef
Klew <nikita.losev200440@gmail.com>
2026-06-25 21:53:58 +1000

    feat(ResultPattern): created

---
+++ ArkWallet/Application/Common/IServiceWorkResult.cs
@@ -0,0 +1,37 @@
+﻿namespace ArkWallet.Application.Common
+{
+    internal record Result<T>
+    {
+        public bool IsSuccess { get; }
+        public string Message { get; }
+        private T? _data { get; }
+
+        private Result(bool isSuccess, string message, T? data)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+            _data = data;
+        }
+
+        public static Result<T> Ok(T data) => new(true, "Success", data);
+        public static Result<T> Fail(string message) => new(false, message, default);
+
+        public bool TryGetData(out T data)
+        {
+            if (IsSuccess && _data is not null)
+            {
+                data = _data;
+                return true;
+            }
+
+            data = default!;
+            return false;
+        }
+    }
+
+    internal record Result(bool IsSuccess, string Message)
+    {
+        public static Result Ok() => new(true, "Success");
+        public static Result Fail(string message) => new(false, message);
+    }
+}
