642b7184e37338fda8794e2224f626f4e14b0b87
Klew <nikita.losev200440@gmail.com>
2026-06-25 19:45:03 +1000

    test(TokenPriceCandleUpdateServiceTest): created test kit

---
+++ ArkWallet.Tests/TestTimeProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace ArkWallet.Tests;
+
+internal class TestTimeProvider : TimeProvider
+{
+    public DateTimeOffset DateTimeOffsetNow { get; set; } = new DateTimeOffset(2026, 1, 1, 0, 0, 0, new TimeSpan());
+
+    public override DateTimeOffset GetUtcNow()
+    {
+        return DateTimeOffsetNow;
+    }
+
+    public void SkipInSeconds(int seconds)
+    {
+        DateTimeOffsetNow = DateTimeOffsetNow.AddSeconds(seconds);
+    }
+}
