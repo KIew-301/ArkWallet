8d6381492a31112cf3591e560d33847f3c1f958d
Klew <nikita.losev200440@gmail.com>
2026-06-26 22:46:10 +1000

    style(global): renamed and structured

--- ArkWallet/Presentation/DTOs/GetOrdersDTO.cs
+++ ArkWallet/Presentation/DTOs/GetOrdersDTO.cs
@@ -2,5 +2,8 @@
 {
     public record GetOrdersRequest(bool IncludeActive, bool IncludeInactive);
     public record GetOrdersResponse(OrderItem[] Orders);
-    public record OrderItem(string OrderId, string Symbol, string TokenName, string Direction, decimal TotalQuantity, decimal FilledQuantity, decimal FillPercent, decimal OrderPrice, decimal CurrentPrice, string IconUrl);
+    public record OrderItem(
+        string OrderId, string Symbol, string TokenName, 
+        string Direction, decimal TotalQuantity, decimal FilledQuantity, 
+        decimal FillPercent, decimal OrderPrice, decimal CurrentPrice, string IconUrl);
 }
