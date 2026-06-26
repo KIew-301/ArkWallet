40d52f30df9917f669410dfd377451b047a6fa7c
Klew <nikita.losev200440@gmail.com>
2026-06-26 21:04:24 +1000

    test(TradersControllerTest): created with mocking

---
+++ ArkWallet.Tests/ApiTests/TradersControllerTest.cs
@@ -0,0 +1,71 @@
+﻿using ArkWallet.Application.Common;
+using ArkWallet.Application.Contracts.TraderServices;
+using ArkWallet.Application.Services.TraderServices;
+using ArkWallet.Presentation.API;
+using ArkWallet.Presentation.DTOs;
+using ArkWallet.Tests.HelpTools;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ArkWallet.Tests.ApiTests;
+
+public class TradersControllerTest
+{
+    [Fact]
+    public async Task TakeBalance_WhenCalcServiceReturnData_ReturnsSuccess()
+    {
+        var mockBalanceChangesCalculationService = new Mock<IBalanceChangesCalculationService>();
+        mockBalanceChangesCalculationService
+            .Setup(x => x.TakeMainBalanceChanges(It.IsAny<long>(), It.IsAny<int>()))
+            .ReturnsAsync(Result<BalanceChangesData>
+                .Ok(new BalanceChangesData(1250, 1000, 250, 25)));
+
+        var traderController = new TradersController(mockBalanceChangesCalculationService.Object);
+        traderController.AddContext("101");
+
+        var result = await traderController.GetBalance(new GetBalanceRequest(5));
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var response = Assert.IsType<GetBalanceResponse>(okResult.Value);
+
+        Assert.Equal(1250, response.CurrentBalance);
+        Assert.Equal(250, response.ChangeAbsolute);
+        Assert.Equal(25, response.ChangePercent);
+    }
+
+    [Fact]
+    public async Task TakeBalance_WhenCalcServiceReturnError_ReturnsBadRequest()
+    {
+        var mockBalanceChangesCalculationService = new Mock<IBalanceChangesCalculationService>();
+        mockBalanceChangesCalculationService
+            .Setup(x => x.TakeMainBalanceChanges(It.IsAny<long>(), It.IsAny<int>()))
+            .ReturnsAsync(Result<BalanceChangesData>
+                .Fail("Calculation service error"));
+
+        var traderController = new TradersController(mockBalanceChangesCalculationService.Object);
+        traderController.AddContext("101");
+
+        var result = await traderController.GetBalance(new GetBalanceRequest(5));
+
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+        var message = badRequestResult.Value;
+
+        Assert.Equal("Calculation service error", message);
+    }
+
+    [Fact]
+    public async Task TakeBalance_WhenContextNotExist_ReturnsUnauthorized()
+    {
+        var mockBalanceChangesCalculationService = new Mock<IBalanceChangesCalculationService>();
+        mockBalanceChangesCalculationService
+            .Setup(x => x.TakeMainBalanceChanges(It.IsAny<long>(), It.IsAny<int>()))
+            .ReturnsAsync(Result<BalanceChangesData>
+                .Fail("Calculation service error"));
+
+        var traderController = new TradersController(mockBalanceChangesCalculationService.Object);
+
+        var result = await traderController.GetBalance(new GetBalanceRequest(5));
+
+        var badRequestResult = Assert.IsType<UnauthorizedResult>(result);
+    }
+}
---
+++ ArkWallet.Tests/HelpTools/ControllerExtention.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ArkWallet.Tests.HelpTools;
+
+public static class ControllerExtention
+{
+    public static void AddContext(this ControllerBase controller, string telegramId)
+    {
+        var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, telegramId) };
+        var identity = new ClaimsIdentity(claims, "TestAuth");
+        var principal = new ClaimsPrincipal(identity);
+        controller.ControllerContext = new() { HttpContext = new DefaultHttpContext { User = principal } };
+    }
+}
