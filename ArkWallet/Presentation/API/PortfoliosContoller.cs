86186b2f8d4bac5afdadec6ec0640f9ae6046934
Klew <nikita.losev200440@gmail.com>
2026-06-30 01:09:10 +1000

    feat(PortfoliosContoller): created

---
+++ ArkWallet/Presentation/API/PortfoliosContoller.cs
@@ -0,0 +1,26 @@
+﻿using ArkWallet.Application.Contracts.PortfolioServices;
+using ArkWallet.Presentation.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ArkWallet.Presentation.API;
+
+[ApiController]
+public class PortfoliosContoller(IPortfolioQueryService portfolioQueryService) : ControllerBase
+{
+    [Authorize]
+    [HttpGet("porfolio")]
+    public async Task<IActionResult> GetPortfolio()
+    {
+        if (!long.TryParse(User?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out var userTelegramId))
+            return Unauthorized();
+
+        var result = await portfolioQueryService.GetTraderTokensAsync(userTelegramId);
+
+        if (result.TryGetData(out var data))
+            return BadRequest(result.Message);
+
+        return Ok(new GetPortfolioResponse(data));
+    }
+}
