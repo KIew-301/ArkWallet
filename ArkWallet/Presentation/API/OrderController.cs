9f0e01316bc8671fd4145f020a4a784ca79a35cf
Klew <nikita.losev200440@gmail.com>
2026-07-01 23:08:06 +1000

    feat(OrderController): created

--- ArkWallet/Presentation/API/OrderController.cs
+++ ArkWallet/Presentation/API/OrderController.cs
@@ -1,16 +1,29 @@
 ﻿using ArkWallet.Application.Contracts.TradeOrderServices;
+using ArkWallet.Presentation.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace ArkWallet.Presentation.API
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class OrderController(IOrderValidationService orderValidationService) : ControllerBase
+    [Route("api/v1/[controller]")]
+    public class OrdersController(IOrderQueryService orderQueryService) : ControllerBase
     {
-        [HttpPost]
-        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderCommand request)
+        [Authorize]
+        [HttpGet("order")]
+        public async Task<IActionResult> Order([FromBody] GetOrdersRequest request)
         {
-            return Ok("Заказ успешно создан!"); // Заглушка для демонстрации
+            if (!long.TryParse(User?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out var userTelegramId))
+                return Unauthorized();
+
+            var result = await orderQueryService.GetTraderOrdersAsync(
+                userTelegramId, request.IncludeActive, request.IncludeFilled, request.IncludeCancelled, true);
+
+            if (!result.TryGetData(out var data))
+                return BadRequest(result.Message);
+
+            return Ok(new GetOrdersResponse(data.ToArray()));
         }
     }
 }
