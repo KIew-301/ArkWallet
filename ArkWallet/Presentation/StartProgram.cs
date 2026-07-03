62b2890a878b34ac545d963c96ba9c210a9e6bc0
Klew <nikita.losev200440@gmail.com>
2026-07-03 20:36:52 +1000

    feat(StartProgram): update with migrations update

--- ArkWallet/Presentation/StartProgram.cs
+++ ArkWallet/Presentation/StartProgram.cs
@@ -1,16 +1,22 @@
 ﻿using ArkWallet.Application.Contracts.CharacterTokenServices;
 using ArkWallet.Application.Contracts.Decorators;
+using ArkWallet.Application.Contracts.MarketMaker;
+using ArkWallet.Application.Contracts.Orchestrators;
 using ArkWallet.Application.Contracts.Other;
 using ArkWallet.Application.Contracts.PortfolioServices;
 using ArkWallet.Application.Contracts.SuggestionServices;
 using ArkWallet.Application.Contracts.TradeOrderServices;
 using ArkWallet.Application.Contracts.TraderServices;
 using ArkWallet.Application.Services.CharacterTokenServices;
+using ArkWallet.Application.Services.FullValidationService;
+using ArkWallet.Application.Services.MarketMaker;
+using ArkWallet.Application.Services.Orchestrators;
 using ArkWallet.Application.Services.Other;
 using ArkWallet.Application.Services.PortfolioServices;
 using ArkWallet.Application.Services.SuggestionServices;
 using ArkWallet.Application.Services.TradeOrderServices;
 using ArkWallet.Application.Services.TraderServices;
+using ArkWallet.Application.Workers;
 using ArkWallet.Domain.Engines;
 using ArkWallet.Entities.Configurations;
 using ArkWallet.Infrastructure;
@@ -78,51 +84,18 @@ class Program
             .AddUserSecrets<Program>()
             .Build();
 
-        builder.Services.AddDbContext<ArkWalletDbContext>(options =>
-            options.UseSqlite("Data Source=arkwallet.db"));
-
         builder.Services.AddSingleton<IConfiguration>(configuration);
 
         // Main
-        builder.Services.AddDbContext<ArkWalletDbContext>();
-        builder.Services.AddScoped<TradingEngine>();
+        builder.Services.AddDbContext<ArkWalletDbContext>(options =>
+            options.UseSqlite("Data Source=arkwallet.db"));
 
-        builder.Services.AddScoped<TelegramBot>();
+        // Services
+        RegisterServices(builder.Services);
 
-        builder.Services.AddScoped<RabbitMQService>();
-        builder.Services.AddScoped<ITaskDispatcher, RabbitMQTaskDispatcher>();
-
+        // Background Services
+        builder.Services.AddHostedService<MarketMakerWorker>();
         builder.Services.AddHostedService<NotificationWorker>();
-
-        builder.Services.AddScoped<ReserveCalculationService>();
-
-        // Wizard
-        builder.Services.AddScoped<WizardConfiguration>();
-        builder.Services.AddScoped<WizardEngine>();
-
-        // CharacterTokenServices
-        builder.Services.AddScoped<ITokenCreationService, TokenCreationService>();
-
-        // Decorators
-        builder.Services.AddScoped<IButtonDecorator, ButtonDecorator>();
-        builder.Services.AddScoped<IQuestionDecorator, QuestionDecorator>();
-
-        // PortfolioServices
-        builder.Services.AddScoped<IPortfolioQueryService, PortfolioQueryService>();
-        builder.Services.AddScoped<IPortfolioUpdatingService, PortfolioUpdatingService>();
-
-        // SuggestionServices
-        builder.Services.AddScoped<IPriceSuggestionService, PriceSuggestionService>();
-        builder.Services.AddScoped<IQuantitySuggestionService, QuantitySuggestionService>();
-
-        // TradeOrderServices
-        builder.Services.AddScoped<IOrderCancellationService, OrderCancellationService>();
-        builder.Services.AddScoped<IOrderCreationService, OrderCreationService>();
-        builder.Services.AddScoped<IOrderValidationService, OrderValidationService>();
-
-        // TraderServices
-        builder.Services.AddScoped<ITraderBalanceUpdatingService, TraderBalanceUpdatingService>();
-        builder.Services.AddScoped<ITraderRegistrationService, TraderRegistrationService>();
 
         var serviceProvider = builder.Services.BuildServiceProvider();
 
@@ -133,14 +106,77 @@ class Program
             Console.WriteLine("Миграции применены!");
         }
 
+        // Telegram Bot
         var bot = serviceProvider.GetRequiredService<TelegramBot>();
         await bot.Start();
 
+        // Hosted Services
         var hostedServices = serviceProvider.GetServices<IHostedService>();
         foreach (var hostedService in hostedServices)
             await hostedService.StartAsync(CancellationToken.None);
 
         Console.ReadLine();
+    }
+
+    private static void RegisterServices(IServiceCollection services)
+    {
+        // DbContext
+        services.AddDbContext<ArkWalletDbContext>();
+
+        // Domain Engines
+        services.AddScoped<TradingEngine>();
+
+        // Telegram Bot
+        services.AddScoped<TelegramBot>();
+
+        // RabbitMQ
+        services.AddScoped<RabbitMQService>();
+        services.AddScoped<ITaskDispatcher, RabbitMQTaskDispatcher>();
+
+        // Wizard
+        services.AddScoped<WizardConfiguration>();
+        services.AddScoped<WizardEngine>();
+
+        // CharacterTokenServices
+        services.AddScoped<ITokenCreationService, TokenCreationService>();
+        services.AddScoped<ITokenPriceCandleUpdateService, TokenPriceCandleUpdateService>();
+        services.AddScoped<ITokenQueryService, TokenQueryService>();
+        services.AddScoped<ITokenPriceChangesCalculationService, TokenPriceChangeCalculationService>();
+
+        // Decorators
+        services.AddScoped<IButtonDecorator, ButtonDecorator>();
+        services.AddScoped<IQuestionDecorator, QuestionDecorator>();
+
+        // PortfolioServices
+        services.AddScoped<IPortfolioQueryService, PortfolioQueryService>();
+        services.AddScoped<IPortfolioUpdatingService, PortfolioUpdatingService>();
+
+        // SuggestionServices
+        services.AddScoped<IPriceSuggestionService, PriceSuggestionService>();
+        services.AddScoped<IQuantitySuggestionService, QuantitySuggestionService>();
+
+        // TradeOrderServices
+        services.AddScoped<IOrderCancellationService, OrderCancellationService>();
+        services.AddScoped<IOrderCreationService, OrderCreationService>();
+        services.AddScoped<IOrderCreationFullValidationService, OrderCreationFullValidationService>();
+        services.AddScoped<IOrderValidationService, OrderValidationService>();
+        services.AddScoped<IOrderQueryService, OrderQueryService>();
+
+        // TraderServices
+        services.AddScoped<ITraderBalanceUpdatingService, TraderBalanceUpdatingService>();
+        services.AddScoped<ITraderRegistrationService, TraderRegistrationService>();
+        services.AddScoped<IBalanceSnapshotService, BalanceSnapshotService>();
+        services.AddScoped<IBalanceChangesCalculationService, BalanceChangesCalculationService>();
+
+        // MarketMaker
+        services.AddScoped<IMarketMakerBotRegistrationService, MarketMakerBotRegistrationService>();
+        services.AddScoped<IMarketMakerOrchestrator, MarketMakerOrchestrator>();
+        services.AddScoped<IMarketMakerOrderService, MarketMakerOrderService>();
+
+        // Other
+        services.AddScoped<ReserveCalculationService>();
+        services.AddScoped<ITraderAuthService, TraderAuthService>();
+        services.AddScoped<ITokenService, TokenService>();
     }
 
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ArkWalletDbContext>
