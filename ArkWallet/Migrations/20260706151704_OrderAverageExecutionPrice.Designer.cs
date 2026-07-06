ae91608b1aa0c96bb69ce80fbf1f1a711eb57426
Klew <nikita.losev200440@gmail.com>
2026-07-07 01:21:20 +1000

    feat(database): add AverageExecutePrice to TradeOrder

--- ArkWallet/Migrations/20260706151704_OrderAverageExecutionPrice.Designer.cs
+++ ArkWallet/Migrations/20260706151704_OrderAverageExecutionPrice.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ArkWallet.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ArkWallet.Migrations
 {
     [DbContext(typeof(ArkWalletDbContext))]
-    partial class ArkWalletDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260706151704_OrderAverageExecutionPrice")]
+    partial class OrderAverageExecutionPrice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
