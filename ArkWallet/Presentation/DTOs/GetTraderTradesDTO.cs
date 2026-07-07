9696ee874f4b7023b96e9fef14f3b8ebd88b86a8
Klew <nikita.losev200440@gmail.com>
2026-07-07 23:46:53 +1000

    feat(TradesController): create

--- ArkWallet/Presentation/DTOs/GetTraderTradesDTO.cs
+++ ArkWallet/Presentation/DTOs/GetTraderTradesDTO.cs
@@ -1,18 +1,10 @@
-﻿namespace ArkWallet.Presentation.DTOs
+﻿using ArkWallet.Application.Contracts.TradeServices;
+
+namespace ArkWallet.Presentation.DTOs
 {
     /// <summary>
     /// Ответ со списком сделок
     /// </summary>
     /// <param name="Trades">Массив сделок</param>
-    public record GetTradesResponse(TradeItem[] Trades);
-    /// <summary>
-    /// Информация о сделке
-    /// </summary>
-    /// <param name="Symbol">Символ токена</param>
-    /// <param name="TraderRole">Роль трейдера (Buyer/Seller)</param>
-    /// <param name="ExecutionPrice">Цена исполнения</param>
-    /// <param name="Quantity">Количество</param>
-    /// <param name="Profit">Прибыль/убыток</param>
-    /// <param name="TradeDateTime">Дата и время сделки</param>
-    public record TradeItem(string Symbol, string TraderRole, decimal ExecutionPrice, decimal Quantity, decimal Profit, DateTime TradeDateTime);
+    public record GetTradesResponse(TradeInfo[] Trades);
 }
