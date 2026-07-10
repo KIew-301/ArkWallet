c8bf5e3c719d57a3e96389be1fd293f3ffe17e31
Klew <nikita.losev200440@gmail.com>
2026-07-11 08:35:43 +1000

    chore(Presentation): add ExcludeFromCodeCoverage to TradesController

--- ArkWallet/Presentation/API/TradesController.cs
+++ ArkWallet/Presentation/API/TradesController.cs
@@ -2,6 +2,7 @@
 using ArkWallet.Presentation.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 using System.Security.Claims;
 
 namespace ArkWallet.Presentation.API;
@@ -9,6 +10,7 @@ namespace ArkWallet.Presentation.API;
 /// <summary>
 /// Контроллер для получения данных о сделках трейдера
 /// </summary>
+[ExcludeFromCodeCoverage(Justification = "API-контроллер: только маршрутизация HTTP-запросов к сервисам. Не содержит бизнес-логики, тестируется интеграционно.")]
 [ApiController]
 [Route("api/v1/[controller]")]
 [Authorize]
