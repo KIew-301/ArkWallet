e7def1129706b9c6fd63703af424d9b67584097a
Klew <nikita.losev200440@gmail.com>
2026-07-11 18:29:20 +1000

    chore(Presentation): add ExcludeFromCodeCoverage to DataSenderService

--- ArkWallet/Presentation/Telegram/DataSenderService.cs
+++ ArkWallet/Presentation/Telegram/DataSenderService.cs
@@ -1,5 +1,8 @@
-﻿namespace ArkWallet.Presentation.Telegram
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ArkWallet.Presentation.Telegram
 {
+    [ExcludeFromCodeCoverage(Justification = "Пустой static class, не содержит логики. Кандидат на удаление как dead code.")]
     static internal class DataSenderService
     {
 
