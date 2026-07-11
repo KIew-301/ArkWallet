e974099e8d61172ad61b95cd123a55d8cfbf5055
Klew <nikita.losev200440@gmail.com>
2026-07-11 18:28:21 +1000

    fix(PortfolioQueryService): use correct parameter name in ArgumentNullException to satisfy S3928/CA2208

--- ArkWallet/Application/Contracts/PortfolioServices/IPortfolioQueryService.cs
+++ ArkWallet/Application/Contracts/PortfolioServices/IPortfolioQueryService.cs
@@ -52,7 +52,7 @@ namespace ArkWallet.Application.Contracts.PortfolioServices
                 throw new ArgumentNullException(nameof(item), $"{MethodBase.GetCurrentMethod()?.Name} - item не может быть null");
 
             if (item.CharacterToken == null)
-                throw new ArgumentNullException(nameof(item.CharacterToken), $"{MethodBase.GetCurrentMethod()?.Name} - item.CharacterToken не может быть null");
+                throw new ArgumentNullException(nameof(item), $"{MethodBase.GetCurrentMethod()?.Name} - item.CharacterToken не может быть null");
 
             var balanceInToken = item.Quantity * item.CharacterToken.CurrentPrice;
             var cost = item.Quantity * item.AverageBuyPrice;
