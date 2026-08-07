71f739ae30f070b6c6b57496b31f56f6fea56d73
Klew <nikita.losev200440@gmail.com>
2026-08-08 05:02:33 +1000

    test(GateBudgets): recalibrate budgets for split-query snapshot reads

--- ArkWallet.PerformanceTests/Gates/GateBudgets.cs
+++ ArkWallet.PerformanceTests/Gates/GateBudgets.cs
@@ -7,7 +7,7 @@ internal static class GateBudgets
     public static readonly Budget TokenQuery50T = new(110, 30, null, 158);
     public static readonly Budget BalanceMainChanges = new(5, 355, null, 4);
     public static readonly Budget BalanceTotalChanges = new(5, 25, null, 4);
-    public static readonly Budget LeadersTop50T = new(110, 50, null, 158);
+    public static readonly Budget LeadersTop50T = new(220, 50, null, 220);
     public static readonly Budget OrderCreateBuy = new(10, 25, null, 7);
     public static readonly Budget OrderCreateSell = new(11, 25, null, 8);
     public static readonly Budget MarketMakerTick10T = new(6000, 3259, 600, 12615);
@@ -17,14 +17,14 @@ internal static class GateBudgets
     public static readonly Budget E2eCacheCheck = new(108, 40, null, 159);
     public static readonly Budget E2eBotWizardFlow = new(238, 4000, null, 369);
     public static readonly Budget E2eBotAdminFlow = new(40, 2000, null, 23);
-    public static readonly Budget E2eTelegramBotLevel = new(102, 40, null, 141);
+    public static readonly Budget E2eTelegramBotLevel = new(210, 40, null, 190);
 
     public static readonly Budget HeavyOrdersGet = new(2, 2000, null, 12000);
     public static readonly Budget HeavyOrderCreate = new(25, 2000, 5, 2500);
     public static readonly Budget HeavyOrdersCancelAll = new(6000, 5000, 5, 10000);
     public static readonly Budget HeavyTokensGet = new(2000, 1000, null, 3000);
     public static readonly Budget HeavyCandleGet = new(2, 3000, null, 120000);
-    public static readonly Budget HeavyBalanceGet = new(10, 15000, null, 1500000);
+    public static readonly Budget HeavyBalanceGet = new(6, 100, null, 2500);
     public static readonly Budget HeavyTradesGet = new(2, 3000, null, 25000);
 
     public static readonly IReadOnlyDictionary<string, Budget> ById = new Dictionary<string, Budget>(StringComparer.OrdinalIgnoreCase)
